e16d62b0e26cc3b890bd9a98b77a1f6be34305b6
ivanvinicius <ivanvinicius@github.com>
2024-06-19 19:25:20 -0300

    error handling

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,21 +1,51 @@
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { errorHandler } from '@/http/error-handler'
+import { getProfile } from '@/http/routes/auth/get-profile'
+
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+app.setErrorHandler(errorHandler)
+
 app.register(fastifyCors)
+app.register(fastifyJwt, {
+  secret: 'secret',
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js saas',
+      description: 'Full stack saas app multi tenant RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 // rotas
 app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(getProfile)
 
 app.listen({ port: 3333 }).then(() => console.log('server running'))
